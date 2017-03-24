b300a7899067ade85baf618051873a65c2ed9f7f
Daniel Song <xdtsong@gmail.com>
2017-03-23 18:00:01 -0700

    Add login request handler to App

--- src/main/java/com/security/UiApplication.java
+++ src/main/java/com/security/UiApplication.java
@@ -2,6 +2,11 @@ package com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +30,21 @@ public class UiApplication {
     @RequestMapping("/user")
     public Principal user(Principal user) {
         return user;
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity
+                    .httpBasic()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated();
+        }
     }
 
 	public static void main(String[] args) {
